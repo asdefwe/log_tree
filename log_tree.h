4a01ca567895e1b65b400f7820f97326698c1b55
何章鹏 <560454@gree.com.cn>
2023-09-28 01:38:13 -0700

    定义功能函数

---
+++ log_tree.h
@@ -0,0 +1,40 @@
+#ifndef __LOG_TREE_H
+#define __LOG_TREE_H
+
+#include "stdio.h"
+
+#define log()  printf()
+
+/*================== log tree kernel ==================*/
+
+
+
+/*================== ringbuffer ==================*/
+
+typedef unsigned char u8;	
+
+
+
+typedef struct{
+	u8 buffer[1000];
+	u8* Read_p;
+	u8* Write_p;
+}log_tree_ringbuffer, *log_tree_ringbuffer_p;
+
+
+
+
+
+/*
+  |
+  */
+// void log_tree_Init(log_tree_ringbuffer_p ringbuffer );
+
+
+
+#endif
+
+
+
+
+
