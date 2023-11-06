bbe8e664dca4f9f11299daf14585ad7d87523bed
何章鹏 <13765672229@gitee.com>
2023-11-05 22:26:24 -0800

    添加log_tree核心结构体

--- log_tree_def.h
+++ log_tree_def.h
@@ -35,15 +35,54 @@ typedef struct{
 
 /*================== log tree ==================*/
 
-// typedef struct{	
-// 	lt_uint8_t layers ;
-// 	lt_uint8_t format;
-// }lt_type_t;
+typedef struct{	
+	lt_uint8_t layers ;
+	lt_uint8_t format;
+}lt_type_t;
 
-// typedef struct{	
-// 	lt_uint8_t layers ;
-// 	lt_uint8_t format;
-// }lt_logtree;
+typedef struct{	
+	lt_uint8_t layers ;
+	lt_uint8_t format;
+}lt_logtree;
+
+
+
+
+/*	
+ |	  [position][function1][line]   log_tree begin
+ |			   |					|				 |      |
+ |			   |					|                |   与输出内容的间隔格式						
+ |			   |                    |        是否显示代码行数
+ |             |			是否显示函数名、
+ |		是否显示文件路径
+*/
+
+// log_tree 的单行格式设置
+typedef struct lt_SingleRowFormat_t
+{
+	uint8_t filename;					  //是否显示文件路径
+	uint8_t function;				 	   //是否显示函数名
+	uint8_t	line;						  		//是否显示代码行数
+	uint8_t	Interval_format;	 //与输出内容的间隔格式
+};
+
+/*
+ |	[function1] log_tree begin
+ | 		   |--[function2] fbbdfbdfbdfbdfb
+ | 		   |     |--[function3] iwqqwe568941
+ | 		   |     |     |--[function4] 489516qwd
+ | 		   |--[funciton2] dqw4896156
+ | 		   |--[funciton2] 4615wdxvc489
+ | 
+ |
+ |
+ |
+ |
+ */
+
+
+
+
 
 /*
   |
