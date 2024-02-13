22b8c5755d4fe0daffe36805364c08ebe89b001a
何章鹏 <13765672229>
2024-02-14 00:19:00 +0800

    修改 build.ps1 脚本

--- build.ps1
+++ build.ps1
@@ -1,7 +1,16 @@
-cmake -G "MinGW Makefiles"
+mkdir build
+
+cd .\build\
+
+cmake .. -G "MinGW Makefiles"
 
 mingw32-make
+
+
+
 ECHO ""
 ECHO "============ log_tree running ============"
 ECHO ""
 .\log_tree.exe
+
+cd ..
